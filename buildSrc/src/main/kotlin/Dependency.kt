37023961c88411ac28feec5daa5a2c7d57a069d8
jakubmosakowski <jmosakowski@spoton.com>
2022-07-09 20:16:38 +0200

    Add hilt dependencies

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -5,6 +5,7 @@ object Versions {
     const val minSdk = 23
     const val targetSdk = 32
     const val gradle = "7.1.3"
+    const val hilt = "2.41"
     const val kotlin = "1.7.10"
     const val navigation = "2.5.0"
 }
@@ -12,4 +13,5 @@ object Versions {
 object Classpaths {
     const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}"
 }
